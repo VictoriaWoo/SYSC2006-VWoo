a24caab9f1d3468faede4f92fbc5f89544c049d7
Victoria Woo <99357070+VictoriaWoo@users.noreply.github.com>
2025-01-22 15:38:05 -0500

    Update recursion.c, completed exercises

--- 2006/lab3_p2_win/lab3_p2_win/recursion.c
+++ 2006/lab3_p2_win/lab3_p2_win/recursion.c
@@ -5,16 +5,34 @@
 #include <math.h>
 #include "recursion.h"
 
-
-
 /* Exercise 4. Return the number of digits in integer n, n >= 0. */
 int num_digits(int n)
 {
-    return -1;
+    if (n < 10)
+    {
+        return 1;
+    }
+    else
+    {
+        return num_digits(n / 10) + 1;
+    }
 }
 
 /* Exercise 5. Return x raised to the power n for n >= 0. */
-double power2(double x, int n){
-    return -1;
-}
+double power2(double x, int n)
+{
+    double halfPower = power2(x, n / 2);
 
+    if (n == 0)
+    {
+        return 1;
+    }
+    else if (n % 2 == 0)
+    {
+        return halfPower * halfPower;
+    }
+    else
+    {
+        return halfPower * halfPower * x;
+    }
+}
